9f0fe37786cb6f29c80fa7965696be3f6f34f704
ndochoa <ndaniel8a@gmail.com>
2014-03-31 12:43:59 -0500

    Added copyright info.

--- zqNotas-entity/src/main/java/com/zysquy/zqnotas/entity/Asignatura.java
+++ zqNotas-entity/src/main/java/com/zysquy/zqnotas/entity/Asignatura.java
@@ -1,3 +1,22 @@
+/*******************************************************************************
+ * Copyright (c) 2014 Zysquy.
+ * All rights reserved. This file is part of zqNotas.
+ *
+ * zqNotas is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as published by
+ * the Free Software Foundation.
+ * 	
+ * zqNotas is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+ *     
+ * Contributors:
+ *     Zysquy - initial implementation
+ *******************************************************************************/
 package com.zysquy.zqnotas.entity;
 
 import java.io.Serializable;
