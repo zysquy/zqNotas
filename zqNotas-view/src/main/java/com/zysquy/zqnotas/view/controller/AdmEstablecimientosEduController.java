d6874530bf2df7a42378b895c783228b1ddbb0c5
ndochoa <ndaniel8a@gmail.com>
2014-05-21 07:13:07 -0500

    Added controller for establecimientos educativos manager

---
+++ zqNotas-view/src/main/java/com/zysquy/zqnotas/view/controller/AdmEstablecimientosEduController.java
@@ -0,0 +1,25 @@
+package com.zysquy.zqnotas.view.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import com.zysquy.zqnotas.entity.EstablecimientoEducativo;
+import com.zysquy.zqnotas.logic.administracion.AdmEstablecimientosEduc;
+
+
+@Controller
+public class AdmEstablecimientosEduController {
+
+	@Autowired
+	private AdmEstablecimientosEduc administradorEstablecimientosEducativos;
+	
+	public void crearEntidadEducativa(EstablecimientoEducativo establecimientoEducativo) {
+		this.administradorEstablecimientosEducativos.crear(establecimientoEducativo);
+	}
+	
+	public List<EstablecimientoEducativo> recuperarEstablecimientosEducativos(Integer page, Integer sizePage) {
+		return this.administradorEstablecimientosEducativos.obtenerTodos(null, null);
+	}
+}
