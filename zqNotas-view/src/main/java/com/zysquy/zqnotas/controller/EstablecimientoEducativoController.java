1b0422e0805a7b8e386aed3e697a825c2b5f1c9b
ndochoa <ndaniel8a@gmail.com>
2014-05-25 00:01:39 -0500

    Added reference to bean of session that contains messages to render in view.

--- zqNotas-view/src/main/java/com/zysquy/zqnotas/controller/EstablecimientoEducativoController.java
+++ zqNotas-view/src/main/java/com/zysquy/zqnotas/controller/EstablecimientoEducativoController.java
@@ -5,21 +5,37 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
+import com.zysquy.view.message.MessageBeanModel;
 import com.zysquy.zqnotas.entity.EstablecimientoEducativo;
 import com.zysquy.zqnotas.logic.administracion.IAdmEstablecimientosEduc;
+
 
 
 @Controller
 public class EstablecimientoEducativoController {
 
+  @Autowired
+  private MessageBeanModel messageModel;
+  
 	@Autowired
 	private IAdmEstablecimientosEduc administradorEstablecimientosEducativos;
 	
 	public void crearEntidadEducativa(EstablecimientoEducativo establecimientoEducativo) {
 		this.administradorEstablecimientosEducativos.crear(establecimientoEducativo);
+		this.messageModel.addInfoMessage("establecimiento.educativo.creado");
 	}
 	
 	public List<EstablecimientoEducativo> recuperarEstablecimientosEducativos(Integer page, Integer sizePage) {
 		return this.administradorEstablecimientosEducativos.obtenerTodos(null, null);
 	}
+
+  public MessageBeanModel getMessageModel() {
+    return messageModel;
+  }
+
+  public void setMessageModel(MessageBeanModel messageModel) {
+    this.messageModel = messageModel;
+  }
+	
+	
 }
