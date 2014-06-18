3eb8dcdfbca22be5514e0dee746e56ba03849059
ndochoa <ndaniel8a@gmail.com>
2014-06-17 19:35:57 -0500

    Add Interface repository for TipoEspecialidad

---
+++ zqNotas-entity/src/main/java/com/zysquy/zqnotas/entity/persistence/TipoEspecialidadRepository.java
@@ -0,0 +1,11 @@
+package com.zysquy.zqnotas.entity.persistence;
+
+import org.springframework.stereotype.Repository;
+
+import com.zysquy.persistence.ParametroRepository;
+import com.zysquy.zqnotas.entity.TipoEspecialidad;
+
+@Repository
+public interface TipoEspecialidadRepository extends ParametroRepository<TipoEspecialidad> {
+
+}
